5b2bf2421cb5aa60e46a2866d9c761d90626e6d5
Wilson Delgado <wilsondelgadomoreno@gmail.com>
2024-11-20 14:54:17 -0500

    Finally App

---
+++ login/MauiProgram.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using SkiaSharp.Views.Maui.Controls.Hosting;
+
+namespace login
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .UseSkiaSharp()
+
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Lato-Bold.ttf", "bold");
+                    fonts.AddFont("Lato-Regular.ttf", "regular");
+                    fonts.AddFont("fontaweasomesolid.otf", "iconfont");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
---
+++ login/login_page.xaml.cs
@@ -0,0 +1,9 @@
+namespace login;
+
+public partial class login_page : ContentPage
+{
+	public login_page()
+	{
+		InitializeComponent();
+	}
+}
